7d1829f3e5de8c69d00b2dfcae01150b886c2458
over <franciscosebastianyjunior@hotmail.com>
2023-11-18 01:51:36 -0300

    Adicionar lista de cadastros

--- Forms/Inicio.Designer.cs
+++ Forms/Inicio.Designer.cs
@@ -28,18 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            Cadastros = new ToolStripMenuItem();
+            CadastroSocio = new ToolStripMenuItem();
+            CadastroDependente = new ToolStripMenuItem();
+            CadastroFornecedor = new ToolStripMenuItem();
+            CadastroEspaco = new ToolStripMenuItem();
+            CadastroAtividade = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { Cadastros });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // Cadastros
+            // 
+            Cadastros.DropDownItems.AddRange(new ToolStripItem[] { CadastroSocio, CadastroDependente, CadastroFornecedor, CadastroEspaco, CadastroAtividade });
+            Cadastros.Name = "Cadastros";
+            Cadastros.Size = new Size(71, 20);
+            Cadastros.Text = "Cadastros";
+            // 
+            // CadastroSocio
+            // 
+            CadastroSocio.Name = "CadastroSocio";
+            CadastroSocio.Size = new Size(180, 22);
+            CadastroSocio.Text = "Sócio";
+            // 
+            // CadastroDependente
+            // 
+            CadastroDependente.Name = "CadastroDependente";
+            CadastroDependente.Size = new Size(180, 22);
+            CadastroDependente.Text = "Dependente";
+            // 
+            // CadastroFornecedor
+            // 
+            CadastroFornecedor.Name = "CadastroFornecedor";
+            CadastroFornecedor.Size = new Size(180, 22);
+            CadastroFornecedor.Text = "Fornecedor";
+            // 
+            // CadastroEspaco
+            // 
+            CadastroEspaco.Name = "CadastroEspaco";
+            CadastroEspaco.Size = new Size(180, 22);
+            CadastroEspaco.Text = "Espaço";
+            // 
+            // CadastroAtividade
+            // 
+            CadastroAtividade.Name = "CadastroAtividade";
+            CadastroAtividade.Size = new Size(180, 22);
+            CadastroAtividade.Text = "Atividade";
             // 
             // Inicio
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Inicio";
             Text = "Inicio";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem Cadastros;
+        private ToolStripMenuItem CadastroSocio;
+        private ToolStripMenuItem CadastroDependente;
+        private ToolStripMenuItem CadastroFornecedor;
+        private ToolStripMenuItem CadastroEspaco;
+        private ToolStripMenuItem CadastroAtividade;
     }
 }
